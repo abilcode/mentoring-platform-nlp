672788a30c7a96e65d6327ec08e70156668d95a4
abilcode <m.nabilfad@gmail.com>
2023-05-21 20:26:38 +0700

    fixing translate api to have the same input as sentiment

--- translator/processing/translate.py
+++ translator/processing/translate.py
@@ -1,27 +1,29 @@
 from googletrans import Translator
 import pandas as pd
+import json
 
 def to_translate(data,dest='en'):
     translator = Translator()
     if isinstance(data, str) == True :
         return translator.translate(data,dest=dest).text
+
+def read_data(data='sample.json'):
+    data = data
+    with open(data, 'r') as file:
+        input = json.load(file)
+   
+    return input
+
+if __name__=="__main__":
+    data = read_data()
+    print('=='*30)
+    for dictionary in data:
+        if 'text' in dictionary:
+            dictionary['translate'] = to_translate(dest='en',data=dictionary['text'])
+
+    for dictionary in data:
+        # Print the contents of the dictionary
+        for key, value in dictionary.items():
+            print(f'{key}: {value}')
     
-    else :
-        data_set = data.copy()
-        #data_set = pd.DataFrame(data_set)
-        translated  = []
-        lang_input  = []
-        lang_output = []
-
-        for item in data_set['input'] :
-            translations = translator.translate(item, dest=dest)
-            translated.append(translations.text)
-            lang_input.append(translations.src)
-            lang_output.append(translations.dest)
-            
-        data_set['lang_input'] = lang_input
-        data_set['translated'] = translated
-        data_set['lang_output']= lang_output
-
-        data_pd = pd.DataFrame(data_set)
-        return data_pd
+    
